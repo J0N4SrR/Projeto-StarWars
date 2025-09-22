1c443c9133d15bd4d60c9ca0042398e138629315
Jonas Ribeiro <krnsjrr@gmail.com.br>
2025-09-22 10:05:06 -0300

    Ajuste dos Services add StarshipService

---
+++ src/main/java/br/edu/ifsp/graphql/service/StarshipService.java
@@ -0,0 +1,23 @@
+package br.edu.ifsp.graphql.service;
+
+import br.edu.ifsp.graphql.model.Starship;
+import br.edu.ifsp.graphql.repository.StarshipRepository;
+
+import java.util.List;
+
+public class StarshipService {
+    private final StarshipRepository starshipRepository;
+
+    public StarshipService(StarshipRepository starshipRepository) {
+        this.starshipRepository = starshipRepository;
+    }
+
+    public List<Starship> getAllStarships() {
+        return starshipRepository.findAllStarships();
+    }
+
+    public Starship createStarship(int id, String name, Double length) {
+        Starship newStarship = new Starship(id, name, length);
+        return starshipRepository.saveStarship(newStarship);
+    }
+}
