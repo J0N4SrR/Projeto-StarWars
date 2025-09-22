ce34f2d56eee96e77e952b257a2f5dd8c5383166
Jonas Ribeiro <krnsjrr@gmail.com.br>
2025-09-22 09:59:35 -0300

    Começando os ajustes dos Services add HumanService

---
+++ src/main/java/br/edu/ifsp/graphql/service/HumanService.java
@@ -0,0 +1,28 @@
+package br.edu.ifsp.graphql.service;
+
+import br.edu.ifsp.graphql.model.Human;
+import br.edu.ifsp.graphql.repository.HumanRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HumanService {
+    private final HumanRepository humanRepository;
+
+    public HumanService(HumanRepository humanRepository) {
+        this.humanRepository = humanRepository;
+    }
+
+    public List<Human> getAllHumans() {
+        return humanRepository.findAllHumans();
+    }
+
+    public Human getHumanById(String id) {
+        return humanRepository.findHumanById(id);
+    }
+
+    public Human createHuman(String name, double height) {
+        Human newHuman = new Human(null, name, new ArrayList<>(), new ArrayList<>(), height);
+        return humanRepository.saveHuman(newHuman);
+    }
+}
